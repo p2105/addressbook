12ea7b1ee9da6c3435f7271e1af57216b58df7d8
prashant g <21.prashant@gmail.com>
2020-11-03 20:30:53 +0100

    first commit

---
+++ src/test/java/addressbook/test_addressbook.java
@@ -0,0 +1,26 @@
+package addressbook;//span[text()='Add a New contact']
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test_addressbook {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://161.35.81.8:8080/addressbook/");
+		Thread.sleep(3000);
+		String btn_title = driver.findElement(By.xpath("//span[text()='Add a New contact']")).getText();
+		Assert.assertTrue("Match button title", btn_title.equalsIgnoreCase("Add a New contact"));
+		System.out.println("Test Completed");
+		driver.close();
+		driver.quit();
+	}
+
+}
